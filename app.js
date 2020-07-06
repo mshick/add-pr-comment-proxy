37694b62d590ff3463ddddc51101fe7c53215e1d
Michael Shick <m@shick.us>
2020-07-06 19:05:27 -0400

    error handling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {HttpClient} = require('@actions/http-client')
-const app = express()
+const {ErrorHandler, AuthError, BadRequestError} = require('express-json-api-error-handler')
 
 const basicAuth = (username = '', password = '', realm = 'protected') => (req, res, next) => {
   const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
@@ -12,38 +12,52 @@ const basicAuth = (username = '', password = '', realm = 'protected') => (req, r
   }
 
   res.set('WWW-Authenticate', `Basic realm="${realm}"`)
-  res.status(401).send('Authentication required.')
+  next(new AuthError('Authentication required.'))
 }
 
-const createComment = async (params) => {
+const createComment = async (http, params) => {
   const {repoToken, owner, repo, issueNumber, body} = params
-
-  const http = new HttpClient('http-client-add-pr-comment-bot')
-
-  const additionalHeaders = {
-    accept: 'application/vnd.github.v3+json',
-    authorization: `token ${repoToken}`,
-  }
 
   return http.postJson(
     `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/comments`,
     {body},
-    additionalHeaders,
+    {
+      accept: 'application/vnd.github.v3+json',
+      authorization: `token ${repoToken}`,
+    },
   )
 }
 
-app.use(basicAuth(process.env.WEBHOOK_SECRET))
+const checkToken = async (http, token) => {
+  if (!token) {
+    return false
+  }
+
+  const response = await http.get(`https://api.github.com/`, {
+    accept: 'application/vnd.github.v3+json',
+    authorization: `token ${token}`,
+  })
+
+  return response.message.statusCode === 200
+}
+
+const app = express()
+
+app.use((req, res, next) => {
+  req.httpClient = new HttpClient('http-client-add-pr-comment-bot')
+  next()
+})
 app.use(bodyParser.json())
+app.use(basicAuth(process.env.WEBHOOK_SECRET))
 
 app.post('/:owner/:repo/issues/:issueNumber/comments', async (req, res, next) => {
   try {
-    // TEST FOR VALID TEMP TOKEN
-    const temporaryGithubToken = req.header('Temporary-Github-Token')
-    if (!temporaryGithubToken) {
-      throw new Error('must provide a temporary github token')
+    const isTokenValid = await checkToken(req.httpClient, req.header('temporary-github-token'))
+    if (!isTokenValid) {
+      throw new BadRequestError('must provide a valid temporary github token')
     }
 
-    const response = await createComment({
+    const response = await createComment(req.httpClient, {
       ...req.params,
       ...req.body,
       repoToken: process.env.GITHUB_TOKEN,
@@ -54,5 +68,10 @@ app.post('/:owner/:repo/issues/:issueNumber/comments', async (req, res, next) =>
     next(err)
   }
 })
+
+// Must use last
+const errorHandler = new ErrorHandler()
+errorHandler.setErrorEventHandler((err) => console.log(err))
+app.use(errorHandler.handle)
 
 app.listen(process.env.PORT || 3000)
