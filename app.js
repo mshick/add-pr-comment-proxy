d9b4e2a67325f42693692a39cdbc8a5d8e91bc5a
Michael Shick <m@shick.us>
2020-07-06 16:53:18 -0400

    basic proxy

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const {HttpClient} = require('@actions/http-client')
 
 const basicAuth = (username = '', password = '', realm = 'protected') => (req, res, next) => {
   const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
@@ -13,10 +15,44 @@ const basicAuth = (username = '', password = '', realm = 'protected') => (req, r
   res.status(401).send('Authentication required.')
 }
 
-app.use(basicAuth(process.env.WEBHOOK_SECRET))
+const createComment = async (params) => {
+  const {repoToken, owner, repo, issueNumber, body} = params
 
-app.get('/', (req, res) => {
-  res.send('you made it!---' + JSON.stringify(process.env))
+  const http = new HttpClient('http-client-add-pr-comment-bot')
+
+  const additionalHeaders = {
+    accept: 'application/vnd.github.v3+json',
+    authorization: `token ${repoToken}`,
+  }
+
+  return http.postJson(
+    `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/comments`,
+    {body},
+    additionalHeaders,
+  )
+}
+
+app.use(basicAuth(process.env.WEBHOOK_SECRET))
+app.use(bodyParser.json())
+
+app.post('/:owner/:repo/issues/:issueNumber/comments', async (req, res, next) => {
+  try {
+    // TEST FOR VALID TEMP TOKEN
+    const temporaryGithubToken = req.header('Temporary-Github-Token')
+    if (!temporaryGithubToken) {
+      throw new Error('must provide a temporary github token')
+    }
+
+    const response = await createComment({
+      ...req.params,
+      body: req.body,
+      repoToken: process.env.GITHUB_TOKEN,
+    })
+
+    res.status(200).send(response).end()
+  } catch (err) {
+    next(err)
+  }
 })
 
 app.listen(process.env.PORT || 3000)
